a3aa470ca66c1a62e1edafeb08560f8d5ebb8ee9
AbacaxiBR <94205920+AbacaxiBR@users.noreply.github.com>
2022-10-28 19:43:52 -0300

    Add files via upload

---
+++ PRO-C43-V3-Projeto-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIAVEL";
+const accessCode2 = "CONSTANTE";
+const accessCode3 = "JAVASCRIPT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E I V R A A V L ", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    fill("white")
+    textSize(15)
+    text("E T C A N N O T ", 700,150)
+    fill("lightblue")
+    text("Dica: Nunca mudando!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("V C S A A R I P T J ", 100,250)
+    fill("lightblue")
+    text("Dica: E uma lingugem de progamacao!!", 100,270)
+}
